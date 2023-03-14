6454f041e7ee5888c0798c20c24842775c0d1a1f
Qiang Xu <vettelx1998@gmail.com>
2023-03-14 11:40:17 +0800

    LeetCode 16/17

--- python/LeetCode015.py
+++ python/LeetCode015.py
@@ -4,12 +4,43 @@ import itertools
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # 3sum
-        result = []
+        res = set()
+        n, z, p = [], [], []
+        for num in nums:
+            if num > 0:
+                p.append(num)
+            elif num < 0:
+                n.append(num)
+            else:
+                z.append(num)
 
-        result.sort()
-        result = list(k for k, _ in itertools.groupby(result))
-        return result
+        N, P = set(n), set(p)
+
+        # 1. (-a,0,a)
+        if len(z) > 0:
+            for num in P:
+                if -1 * num in N:
+                    res.add((-num, 0, num))
+
+        # 2.(0,0,0)
+        if len(z) >= 3:
+            res.add((0, 0, 0))
+
+        # 3. (-a,-b,a+b)
+        for i in range(len(n)):
+            for j in range(i + 1, len(n)):
+                target = -(n[i] + n[j])
+                if target in P:
+                    res.add(tuple(sorted((n[i], n[j], target))))
+
+        # 4. (-a-b,a,b)
+        for i in range(len(p)):
+            for j in range(i + 1, len(p)):
+                target = -(p[i] + p[j])
+                if target in N:
+                    res.add(tuple(sorted((p[i], p[j], target))))
+
+        return res
 
 
 if __name__ == "__main__":
--- python/LeetCode017.py
+++ python/LeetCode017.py
@@ -1,8 +1,56 @@
 from typing import List
+import itertools
+
+
+map = {
+    "2": ["a", "b", "c"],
+    "3": ["d", "e", "f"],
+    "4": ["g", "h", "i"],
+    "5": ["j", "k", "l"],
+    "6": ["m", "n", "o"],
+    "7": ["p", "q", "r", "s"],
+    "8": ["t", "u", "v"],
+    "9": ["w", "x", "y", "z"],
+}
+
+dic = {
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
+}
 
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        map = {"2", ["", "", ""]}
-        result = []
+        if len(digits) == 0:
+            return []
+        all_list = []
+        for c in digits:
+            all_list.append(map.get(c))
+        result = ["".join(p) for p in itertools.product(*all_list)]
         return result
+
+    def method2(self, digits: str) -> List[str]:
+        result = []
+        if not digits:
+            return result
+        self.dfs(digits, 0, "", result)
+        return result
+
+    def dfs(self, digits, index, path, res):
+        if index >= len(digits):
+            res.append(path)
+            return
+        tmp = dic.get(digits[index])
+        for c in tmp:
+            self.dfs(digits, index + 1, path + c, res)
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.method2("234"))
